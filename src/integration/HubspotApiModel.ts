3977f357c8a9b018f835570b2edc27b8c297e2b8
daniel-czz <chenzhangzec@126.com>
2021-07-10 12:17:28 +0800

    add new functions in HubspotApiModel and SaasquatchApimodel

--- src/integration/HubspotApiModel.ts
+++ src/integration/HubspotApiModel.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export class HubspotApiModel {
 
-    
+
     // Temp access until DB has OAuth access tokens
     private HAPIKEY: string;
 
@@ -12,7 +12,7 @@ export class HubspotApiModel {
 
     /**
      * Gets contact from HubSpot.
-     * 
+     *
      * @param objectId objectID of contact to query
      * @param paramToGet query parameters to filter by. eg. 'email'.
      */
@@ -42,7 +42,7 @@ export class HubspotApiModel {
 
     /**
      * Creates an object in Hubspot
-     * 
+     *
      * @param objectType object type to be created. e.g. deals, contacts, company
      * @param createObjectBody body specifiying the create properties of the object
      * @returns axios response
@@ -51,7 +51,7 @@ export class HubspotApiModel {
         try{
             const createObjectURL = 'https://api.hubapi.com/crm/v3/objects/' + objectType;
             const response = await axios.post(createObjectURL, createObjectBody,{
-        
+
                 params: {
                     hapikey: this.HAPIKEY
                 }
@@ -63,10 +63,10 @@ export class HubspotApiModel {
             return JSON.parse(e.response.body);
         }
     }
-   
+
 
     /**
-     * 
+     *
      * @param objectType object to search for. e.g. deals, contacts, company
      * @param body body specifiying the search properties of the object
      * @returns axios response
@@ -87,7 +87,44 @@ export class HubspotApiModel {
         }
     }
 
+    /**
+     * get all users in hubspot
+     * @returns axios response
+     */
+    public async getAllContacts(){
+        try {
+            const allContactsURL = 'https://api.hubapi.com/contacts/v1/lists/all/contacts/all?hapikey=' + this.HAPIKEY;
+            const response = await axios.get(allContactsURL);
+            const data = response.data;
+            console.log(data);
+            return data;
+        } catch (e) {
+            console.error("===== WAS NOT ABLE TO get the Hubspor user list: ");
+            console.error(e);
+            return JSON.parse(e.response.body);
+        }
+    }
 
-   
+    /**
+     *Search for a user in Hubspot based on user eamil address
+     */
+    public async searchUser(emailAdress:string){
+        try {
+            //                     https://api.hubapi.com/contacts/v1/search/query?q=rr@uvic.ca&hapikey=8a96de23-fb7e-47be-92de-2a37b60e6450
+            const searchUserURL = 'https://api.hubapi.com/contacts/v1/search/query?q=' + emailAdress + '&hapikey=' + this.HAPIKEY;
+            const response = await axios.get(searchUserURL);
+            const data = response.data; // store the target users information
+            // console.log(data);
+
+            return data;
+        } catch (e) {
+            console.error("===== WAS NOT ABLE TO get the Hubspor user list: ");
+            console.error(e);
+            return JSON.parse(e.response.body);
+        }
+    }
+
+
+
 
 }
